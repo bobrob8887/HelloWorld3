22eb6080f1766a7593aacca86d92db19b05b624d
Maurice Robert <Maurice@Training02-PC.tasco.net>
2020-02-19 16:17:19 +0800

    Initial commit

---
+++ app/src/main/java/com/example/helloworld/Driver.java
@@ -0,0 +1,7 @@
+package com.example.helloworld;
+
+public class Driver {
+
+
+}
+
---
+++ app/src/main/java/com/example/helloworld/Word.java
@@ -0,0 +1,20 @@
+package com.example.helloworld;
+
+
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "word_table")
+public class Word {
+
+    @PrimaryKey
+    @NonNull
+    @ColumnInfo(name = "word")
+    private String mWord;
+
+    public Word(String word) {this.mWord = word;}
+
+    public String getWord(){return this.mWord;}
+}
